06ccc4ecbe40f282ba8d1b1475609b3764bdca62
Nathan Perfetti <nathanperfetti@gmail.com>
2021-10-18 18:07:13 -0500

    added week 17 day 3 content

--- bootcamp_activities/activities17.2/11-Ins_Higher-Order-Functions/index.js
+++ bootcamp_activities/activities17.2/11-Ins_Higher-Order-Functions/index.js
@@ -2,6 +2,7 @@ const arrayIndex = [1, 52, 35, 6, 72, 7, 3, 19, 32, 54, 78, 95, 97];
 
 // Create a function which accepts two arguments, number and index.
 // The index of the array is provided to us because we will pass this function through .map array method.
+
 function evenIndexMultiplier(number, index) {
   // Checking to see if we are on an even index inside of the array we are mapping through.
   if (index % 2 === 0) {
@@ -16,4 +17,5 @@ function evenIndexMultiplier(number, index) {
 // Create a new variable to store our newly mapped array with our `evenIndexMultiplier` function passed through it.
 const evenIndexes = arrayIndex.map(evenIndexMultiplier);
 
-console.log(evenIndexes);
+console.log("Original Arr:",arrayIndex )
+console.log("Even Indexes: ", evenIndexes);
