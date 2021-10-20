3d1379897b9835ffbd596a49704f58fb9f1f9c4d
Chris Woolcott <chris.woolcott@quotacy.com>
2021-10-20 17:11:30 -0500

    18.1 activities

--- bootcamp_activities/activities17.1/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
+++ bootcamp_activities/activities17.1/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
@@ -1,25 +1,26 @@
 // 1) Where is carNoise stored?
-//
+// GEC
 const carNoise = 'Honk';
 // 2) Where is goFast stored?
-// 
+// GEC
 const goFast = speed => {
   // 4) When is speed assigned a value? Where is this value stored?
-  // 
-  
+  // Functional execution context of goFast
+
   // 5) Where is makeNoise stored?
-  //
-  const makeNoise = sound => {
+  // Functional execution context of goFast
+  const makeNoise = function (sound) {
+    var sound2 = true;
     console.log(`My speed is at ${speed}, time to ${sound}`);
   }
 
   // 6) What happens in the following statement?
-  // 
+  // carNoise is assigned to functional execution context of makeNoise
   makeNoise(carNoise);
 }
 
 // 3) What happens in the following statement?
-// 
-if(confirm("Do you want to go fast?")) {
+// Once confirmed, goFast is placed in the callback queue, 
+if (confirm("Do you want to go fast?")) {
   goFast(80);
 }
--- bootcamp_activities/activities17.1/04-Stu_Functional-Execution-Context/Inclass/index.js
+++ bootcamp_activities/activities17.1/04-Stu_Functional-Execution-Context/Inclass/index.js
@@ -2,12 +2,13 @@
 function avg(array) {
   function sum() {
     let total = 0;
-    for(let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       total += array[i];
     }
+    return total;
   }
-  
-  return total/array.length;
+  let newTotal = sum();
+  return newTotal / array.length;
 }
 
 module.exports = avg;
