336c85ad4f73a51b8288fbd53f344f0f31e153b1
Chris Woolcott <chris.woolcott@quotacy.com>
2021-11-15 23:31:01 -0600

    21.3

---
+++ bootcamp_activities/activities21.3/22-Stu_Sign-JWT/Inclass/schemas/resolvers.js
@@ -0,0 +1,87 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('thoughts');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { thoughtId }) => {
+      return Thought.findOne({ _id: thoughtId });
+    },
+  },
+
+  Mutation: {
+    // TODO: Add comments to each line of code below to describe the functionality below
+    addUser: async (parent, args) => {
+      // Create User
+      const user = await User.create(args);
+      // Sign Token to the new user, less fraction to the user, auto sign in. 
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // TODO: Add comments to each line of code below to describe the functionality below
+    login: async (parent, { email, password }) => {
+      // Lookup user by email
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      //if a user is found... compares password using bcrypt
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // signed token is returned
+      const token = signToken(user);
+      return { token, user };
+    },
+    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
+      const thought = await Thought.create({ thoughtText, thoughtAuthor });
+
+      await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        { $addToSet: { thoughts: thought._id } }
+      );
+
+      return thought;
+    },
+    addComment: async (parent, { thoughtId, commentText, commentAuthor }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        {
+          $addToSet: { comments: { commentText, commentAuthor } },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeThought: async (parent, { thoughtId }) => {
+      return Thought.findOneAndDelete({ _id: thoughtId });
+    },
+    removeComment: async (parent, { thoughtId, commentId }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $pull: { comments: { _id: commentId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
